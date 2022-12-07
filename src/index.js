dcb86a07e33463103d561d99505b30450ad7ed52
devbeta8t3 <sehoan30@gmail.com>
2022-12-07 16:04:46 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}..</h2>
+//     </div>
+//   );
+// }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  //컴포넌트 출력물이 DOM에 렌더링 된 후에 실행
+  componentDidMount() {
+    console.log(`컴포넌트가 DOM에 마운트 됨`);
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  //컴포넌트가 DOM에 언마운트 될 때 실행
+  componentWillUnmount() {
+    console.log(`컴포넌트가 DOM에 언마운트 됨`);
+    clearInterval(this.timerID);
+  }
+    
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}...</h2>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Clock />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// function tick() {
+   
+//   root.render(
+//     <Clock  />
+//   );
+// }
+// setInterval(tick, 1000)
+
